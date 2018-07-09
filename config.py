8fee129104793e943a477f01d8984fcbd3d2fa78
ZubyWasTaken <38223403+ZubyWasTaken@users.noreply.github.com>
2018-07-09 18:44:41 +0100

    created config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = 'BOT TOKEN'
+YOUR_OWN_USERNAME_ID = 'YOUR USER ID'
+YOUR_OWN_USERNAME_ID = 'YOUR USER ID'
+
+# In my bot I have used the usernams zubyID and nigelID
+# Replace those names with whatever you name it
